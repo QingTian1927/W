bc91f126c49cf9cae3b1a131c738c2af4a119788
QingTian1927 <120935290+QingTian1927@users.noreply.github.com>
2025-02-01 16:59:43 +0700

    initial commit

---
+++ src/test/java/com/github/qingtian1927/w/WApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.qingtian1927.w;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
