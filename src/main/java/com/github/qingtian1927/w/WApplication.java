e0e143c1e0a0dddcd774c8db7ef2639180d95b7c
QingTian1927 <120935290+QingTian1927@users.noreply.github.com>
2025-03-04 18:33:09 +0700

    chore: updated the main class.
    * Enabled Jpa Auditing for the automatic timestamp creation of entities.

--- src/main/java/com/github/qingtian1927/w/WApplication.java
+++ src/main/java/com/github/qingtian1927/w/WApplication.java
@@ -2,8 +2,11 @@ package com.github.qingtian1927.w;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class WApplication {
 
     public static void main(String[] args) {
