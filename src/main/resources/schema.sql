532ca8a9fa190d38f9b6a2d4bcabc6116f9f0300
QingTian1927 <120935290+QingTian1927@users.noreply.github.com>
2025-03-16 16:17:02 +0700

    refactor: reformatted schema.sql.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,60 +1,77 @@
-IF NOT EXISTS (SELECT * FROM sys.fulltext_catalogs WHERE name = 'PostFTCat')
+IF NOT EXISTS (SELECT *
+               FROM sys.fulltext_catalogs
+               WHERE name = 'PostFTCat')
     BEGIN
         CREATE FULLTEXT CATALOG PostFTCat;
     END
 
-IF NOT EXISTS (
-    SELECT * FROM sys.indexes WHERE name = 'idx_posts_id' AND object_id = OBJECT_ID('posts')
-)
+IF NOT EXISTS (SELECT *
+               FROM sys.indexes
+               WHERE name = 'idx_posts_id'
+                 AND object_id = OBJECT_ID('posts'))
     BEGIN
-        CREATE UNIQUE INDEX idx_posts_id ON posts(id);
+        CREATE UNIQUE INDEX idx_posts_id ON posts (id);
     END
 
-IF NOT EXISTS (SELECT * FROM sys.fulltext_indexes WHERE OBJECT_NAME(object_id) = 'posts')
+IF NOT EXISTS (SELECT *
+               FROM sys.fulltext_indexes
+               WHERE OBJECT_NAME(object_id) = 'posts')
     BEGIN
-        CREATE FULLTEXT INDEX ON posts(content)
+        CREATE FULLTEXT INDEX ON posts (content)
             KEY INDEX idx_posts_id
             ON PostFTCat;
     END
 
 -- # -------------------------------------------------------------------------------------------------------------------
 
-IF NOT EXISTS (SELECT * FROM sys.fulltext_catalogs WHERE name = 'CommentFTCat')
+IF NOT EXISTS (SELECT *
+               FROM sys.fulltext_catalogs
+               WHERE name = 'CommentFTCat')
     BEGIN
         CREATE FULLTEXT CATALOG CommentFTCat;
     END
 
-IF NOT EXISTS (
-    SELECT * FROM sys.indexes WHERE name = 'idx_comments_id' AND object_id = OBJECT_ID('comments')
-)
+IF NOT EXISTS (SELECT *
+               FROM sys.indexes
+               WHERE name = 'idx_comments_id'
+                 AND object_id = OBJECT_ID('comments'))
     BEGIN
-        CREATE UNIQUE INDEX idx_comments_id ON comments(id);
+        CREATE UNIQUE INDEX idx_comments_id ON comments (id);
     END
 
-IF NOT EXISTS (SELECT * FROM sys.fulltext_indexes WHERE OBJECT_NAME(object_id) = 'comments')
+IF NOT EXISTS (SELECT *
+               FROM sys.fulltext_indexes
+               WHERE OBJECT_NAME(object_id) = 'comments')
     BEGIN
-        CREATE FULLTEXT INDEX ON comments(content)
+        CREATE FULLTEXT INDEX ON comments (content)
             KEY INDEX idx_comments_id
             ON CommentFTCat;
     END
 
 -- # -------------------------------------------------------------------------------------------------------------------
 
-IF NOT EXISTS (SELECT * FROM sys.fulltext_catalogs WHERE name = 'ProfileFTCat')
+IF NOT EXISTS (SELECT *
+               FROM sys.fulltext_catalogs
+               WHERE name = 'ProfileFTCat')
     BEGIN
         CREATE FULLTEXT CATALOG ProfileFTCat;
     END
 
-IF NOT EXISTS (
-    SELECT * FROM sys.indexes WHERE name = 'idx_profiles_id' AND object_id = OBJECT_ID('profiles')
-)
+IF NOT EXISTS (SELECT *
+               FROM sys.indexes
+               WHERE name = 'idx_profiles_id'
+                 AND object_id = OBJECT_ID('profiles'))
     BEGIN
-        CREATE UNIQUE INDEX idx_profiles_id ON profiles(user_id);
+        CREATE UNIQUE INDEX idx_profiles_id ON profiles (user_id);
     END
 
-IF NOT EXISTS (SELECT * FROM sys.fulltext_indexes WHERE OBJECT_NAME(object_id) = 'profiles')
+IF NOT EXISTS (SELECT *
+               FROM sys.fulltext_indexes
+               WHERE OBJECT_NAME(object_id) = 'profiles')
     BEGIN
-        CREATE FULLTEXT INDEX ON profiles(bio)
+        CREATE FULLTEXT INDEX ON profiles (bio)
             KEY INDEX idx_profiles_id
             ON ProfileFTCat;
     END
+
+-- # -------------------------------------------------------------------------------------------------------------------
