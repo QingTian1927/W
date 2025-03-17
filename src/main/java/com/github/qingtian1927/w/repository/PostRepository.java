e4683ec315f05299635f06eedb798d2f4a2cad7b
QingTian1927 <120935290+QingTian1927@users.noreply.github.com>
2025-03-17 09:05:04 +0700

    feat: get post from followed users. * The home page now displays posts from followed user, for logged-in users. For guests, all posts are displayed. * Added the explore page which just displays all posts.

--- src/main/java/com/github/qingtian1927/w/repository/PostRepository.java
+++ src/main/java/com/github/qingtian1927/w/repository/PostRepository.java
@@ -5,6 +5,8 @@ import com.github.qingtian1927.w.model.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -17,4 +19,7 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findByRepost(Post post);
     int countByRepost(Post post);
     List<Post> findTop10ByOrderByCreatedDateDesc();
+
+    @Query("SELECT p FROM Post p JOIN Follow f ON p.user = f.followed AND f.follower = :follower OR p.user = :follower")
+    Page<Post> findAllFromFollowed(Pageable pageable, @Param("follower") User follower);
 }
